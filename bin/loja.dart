b6cc3769365ef5d3e937ec04e75fe6f3e347749f
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-09-06 16:22:46 -0300

    Menu

--- bin/loja.dart
+++ bin/loja.dart
@@ -1,5 +1,48 @@
-import 'package:loja/loja.dart' as loja;
+import 'dart:io';
+
+import 'package:loja/src/menu.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${loja.calculate()}!');
+  bool condicao = true;
+
+  do {
+    menu();
+    String? opcao = stdin.readLineSync();
+
+    if (opcao != null) {
+      switch (int.parse(opcao)) {
+        case 1:
+          // cadastrarLivro();
+          break;
+        case 2:
+          // atualizarLivro();
+          break;
+        case 3:
+          // comprar();
+          break;
+        case 4:
+          // cadastrarFuncionario();
+          break;
+        case 5:
+          // atualizarFuncionario();
+          break;
+        case 6:
+          // demitir();
+          break;
+        case 7:
+          // consultarLivros();
+          break;
+        case 8:
+          // consultarFuncionarios();
+          break;
+        case 0:
+          print("Obrigado pela preferência, volte sempre!");
+          condicao = false;
+          break;
+        default:
+          print("Opção inválida!");
+          break;
+      }
+    }
+  } while (condicao);
 }
---
+++ lib/src/menu.dart
@@ -0,0 +1,11 @@
+void menu() {
+  print("| 1 - Cadastrar livro");
+  print("| 2 - Atualizar livro");
+  print("| 3 - Comprar livro");
+  print("| 4 - Cadastrar funcionário");
+  print("| 5 - Atualizar funcionário");
+  print("| 6 - demitir funcionário");
+  print("| 7 - Exibir estoque");
+  print("| 8 - Consultar funcionários");
+  print("| 0 - sair");
+}
