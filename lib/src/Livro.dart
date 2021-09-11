64047c0894b3f112229b59107ec714514d940fb7
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-09-11 14:31:36 -0300

    Fix: Livros

--- lib/src/Livro.dart
+++ lib/src/Livro.dart
@@ -6,7 +6,8 @@ class Livro {
   var preco;
   var quantidade;
 
-  Livro({var id, var titulo, var autor, var editora, var preco, var quantidade}) {
+  Livro(
+      {var id, var titulo, var autor, var editora, var preco, var quantidade}) {
     this.id = id;
     this.titulo = titulo;
     this.autor = autor;
@@ -26,17 +27,9 @@ class Livro {
     this.id = id;
   }
 
-  int getID() {
-    return id;
-  }
-
   // Título
   void setTitulo(String titulo) {
     this.titulo = titulo;
-  }
-
-  String getTitulo() {
-    return titulo;
   }
 
   // Autor
@@ -44,17 +37,9 @@ class Livro {
     this.autor = autor;
   }
 
-  String getAutor() {
-    return autor;
-  }
-
   // Editora
   void setEditora(String editora) {
     this.editora = editora;
-  }
-
-  String getEditora() {
-    return editora;
   }
 
   // Preço
@@ -64,18 +49,10 @@ class Livro {
     }
   }
 
-  double getPreco() {
-    return preco;
-  }
-
   // Quantidade
   void setQuantidade(int quantidade) {
     if (quantidade >= 0) {
       this.quantidade = quantidade;
     }
-  }
-
-  int getQuantidade() {
-    return quantidade;
   }
 }
